5ba852419367c74e19888809ac4ab64ad679b968
Gaëtan Pralong <gaetanpralong@mail.com>
2016-06-14 14:31:43 +0200

    Initial commit.

---
+++ FormLogin.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/**
+ * class FormLogin
+ * 
+ */
+class FormLogin
+{
+
+  /** Aggregations: */
+
+  /** Compositions: */
+
+   /*** Attributes: ***/
+
+  /**
+   * 
+   * @access public
+   */
+  public $email;
+
+  /**
+   * 
+   * @access public
+   */
+  public $pass;
+
+
+
+
+
+
+} // end of FormLogin
+?>
